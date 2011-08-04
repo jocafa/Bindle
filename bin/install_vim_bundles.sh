f4e73c710b71e7bc7be02b71b6371d8675846e99
Josh Faul <josh.faul@gmail.com>
2011-08-04 15:10:56 -0600

    added more colorschemes

--- bin/install_vim_bundles.sh
+++ bin/install_vim_bundles.sh
@@ -27,9 +27,18 @@ get_bundle scrooloose nerdtree
 get_bundle tpope vim-git
 get_bundle tpope vim-haml
 get_bundle tpope vim-markdown
-get_bundle tomasr molokai
 get_bundle wavded vim-stylus
 get_bundle vim-scripts actionscript.vim--Jethani
+
+#colorschemes
 get_bundle vim-scripts eclm_wombat.vim
+get_bundle vim-scripts paintbox
+get_bundle vim-scripts rdark
+get_bundle vim-scripts rdark-terminal
+get_bundle vim-scripts pyte
+get_bundle vim-scripts silent.vim
+get_bundle vim-scripts TuttiColori-Colorscheme
+get_bundle vim-scripts Warm-grey
+get_bundle tomasr molokai
 
 vim -c 'call pathogen#helptags()|q'
