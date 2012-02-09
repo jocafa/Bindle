92760a5b5c4a0f4d4f2d123e44065b6328ee971a
Josh Faul <josh.faul@gmail.com>
2012-02-09 12:31:43 -0700

    more bundles

--- bin/install_vim_bundles.sh
+++ bin/install_vim_bundles.sh
@@ -13,39 +13,45 @@ get_bundle() {
   )
 }
 
-get_bundle leshill vim-json
+get_bundle danchoi ruby_bashrockets.vim
+get_bundle digitaltoad vim-jade
 get_bundle duff vim-bufonly
-get_bundle kchmck vim-coffee-script
-get_bundle jayferd eco.vim
-get_bundle mileszs ack.vim
-#get_bundle pangloss vim-javascript
-get_bundle mattn zencoding-vim
 get_bundle ervandew supertab
+get_bundle jayferd eco.vim
+get_bundle jdevera vim-protobuf-syntax
+get_bundle kchmck vim-coffee-script
+get_bundle leshill vim-json
+get_bundle mattn zencoding-vim
+get_bundle mileszs ack.vim
 get_bundle msanders snipmate.vim
-get_bundle scrooloose snipmate-snippets
+#get_bundle pangloss vim-javascript
 get_bundle scrooloose nerdcommenter
 get_bundle scrooloose nerdtree
+get_bundle scrooloose snipmate-snippets
+get_bundle tpope vim-fugitive
 get_bundle tpope vim-git
 get_bundle tpope vim-haml
 get_bundle tpope vim-markdown
-get_bundle tpope vim-fugitive
-get_bundle digitaltoad vim-jade
-get_bundle wavded vim-stylus
+get_bundle tpope vim-rails
+get_bundle tpope vim-repeat
+get_bundle tpope vim-surround
+#get_bundle vim-scripts Command-T
+get_bundle vim-scripts Indent-Finder
+#get_bundle vim-scripts IndentAnything
 get_bundle vim-scripts JavaScript-Indent
-get_bundle jboyens vim-protobuf
-#get_bundle vim-scripts actionscript.vim--Jethani
+get_bundle wavded vim-stylus
 
 #colorschemes
-get_bundle vim-scripts eclm_wombat.vim
-get_bundle vim-scripts paintbox
-get_bundle vim-scripts rdark
-get_bundle vim-scripts rdark-terminal
-get_bundle vim-scripts pyte
-get_bundle vim-scripts silent.vim
+get_bundle altercation vim-colors-solarized
+get_bundle tomasr molokai
 get_bundle vim-scripts TuttiColori-Colorscheme
 get_bundle vim-scripts Warm-grey
+get_bundle vim-scripts eclm_wombat.vim
+get_bundle vim-scripts paintbox
+get_bundle vim-scripts pyte
+get_bundle vim-scripts rdark
+get_bundle vim-scripts rdark-terminal
+get_bundle vim-scripts silent.vim
 get_bundle vim-scripts vanzan_color
-get_bundle tomasr molokai
-get_bundle altercation vim-colors-solarized
 
 vim -c 'call pathogen#helptags()|q'
